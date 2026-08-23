13166373cf1ca3bc13850869684ca04cdc3942fd
mabisri4-18 <mabisri4@gmail.com>
2026-08-23 22:35:52 +0530

    LeetCode: update cpp/sort_colors.cpp

---
+++ cpp/sort_colors.cpp
@@ -0,0 +1,34 @@
+// ======================================
+// LeetCode Problem: sort colors
+// Language: cpp
+// Link: https://leetcode.com/problems/sort-colors/
+// Synced by: LinkCode
+// Date: 8/23/2026, 10:35:50 PM
+// ======================================
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int zero = 0, two = n-1;
+        int move = 0;
+        while(move<=two)
+        {
+            if(nums[move]==2)
+            {
+                swap(nums[move],nums[two]);
+                two--;
+            }
+            else if(nums[move]==0)
+            {
+                swap(nums[move],nums[zero]);
+                zero++,move++;
+            }
+            else
+            {
+                move++;
+            }
+        }
+    }
+};
