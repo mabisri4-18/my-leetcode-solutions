c2b15055f3818a9b36389fff653a513d6acae605
mabisri4-18 <mabisri4@gmail.com>
2026-09-05 20:21:17 +0530

    LeetCode: update cpp/smallest_stable_index_i.cpp

---
+++ cpp/smallest_stable_index_i.cpp
@@ -0,0 +1,33 @@
+// ======================================
+// LeetCode Problem: smallest stable index i
+// Language: cpp
+// Link: https://leetcode.com/problems/smallest-stable-index-i/
+// Synced by: LinkCode
+// Date: 9/5/2026, 8:21:16 PM
+// ======================================
+
+
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int>suf(n);
+        suf[n-1] = nums[n-1];
+        int maxi = nums[0];
+        int mini = INT_MIN;
+        for(int i=n-2;i>=0;i--)
+        {
+            suf[i] = min(suf[i+1],nums[i]);
+        }
+        for(int i=0;i<n;i++)
+        {
+            maxi = max(maxi,nums[i]);
+            int ans = maxi-suf[i];
+            if(ans<=k)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
